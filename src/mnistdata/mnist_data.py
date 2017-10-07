5a9d368bc2184d30a6aec7d37dc091265af2aea2
lijian2033 <lijian2033@qq.com>
2017-10-07 15:48:24 +0800

    update

--- src/mnistdata/mnist_data.py
+++ src/mnistdata/mnist_data.py
@@ -13,9 +13,9 @@ y=tf.nn.softmax(tf.matmul(x, W)+b)
 y_=tf.placeholder(tf.float32,[None,10])
 cross_entropy=tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y),reduction_indices=[1]))
 
-#����ݶ��½��Ż�
+
 train_step=tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-#ȫ���Ż�������
+
 tf.global_variables_initializer().run()
 
 for i in range(100):
